ead5223ca1d6ad5058d36f7904a6c342172ff909
RachelJDev <rachelhuezojimenez@gmail.com>
2026-05-01 18:14:49 -0600

    Agrego responsive a Landing page

--- JS/Login.js
+++ JS/Login.js
@@ -1,6 +1,7 @@
 let usuario = document.getElementById("usuario")
 let contrasena = document.getElementById("contrasena")
 let boton = document.getElementById("btnLogin")
+let ojito = document.getElementById("ojito")
 
 let errorUsuario = document.getElementById("errorUsuario")
 let errorContrasena = document.getElementById("errorContrasena")
@@ -9,22 +10,39 @@ boton.addEventListener("click", function(e) {
 
 e.preventDefault()
 
+
+ojito.addEventListener("click", function() {
+    if (contrasena.type === "password") {
+        contrasena.type = "text"
+        ojito.classList.remove("fa-eye")
+        ojito.classList.add("fa-eye-slash")
+    } else {
+        contrasena.type = "password"
+        ojito.classList.remove("fa-eye-slash")
+        ojito.classList.add("fa-eye")
+    }
+})
+
 let valido = true
  if (usuario.value === "") {
         errorUsuario.textContent = "⚠️ El Usuario es obligatorio"
         errorUsuario.style.color = "red"
+         usuario.style.borderBottom = "2px solid red"
         valido = false
     } else {
         errorUsuario.textContent = ""
+        usuario.style.borderBottom = ""
     }
 
     // --- VALIDACIÓN DEL APELLIDO ---
     if (contrasena.value === "") {
         errorContrasena.textContent = "⚠️ La Contrasena es obligatoria"
         errorContrasena.style.color = "red"
+         contrasena.style.borderBottom = "2px solid red"
         valido = false
     } else {
         errorContrasena.textContent = ""
+        contrasena.style.borderBottom = ""
     }
 if (valido === true) {
         window.location.href = "Index.html"
--- JS/Registro.js
+++ JS/Registro.js
@@ -4,10 +4,27 @@ let correo = document.getElementById("correo")
 let contraseña = document.getElementById("contraseña")
 let boton = document.getElementById("btnRegistro")
 
+
 let errorNombre = document.getElementById("errorNombre")
 let errorApellido = document.getElementById("errorApellido")
 let errorCorreo = document.getElementById("errorCorreo")
-let errorContraseña = document.getElementById("errorContraseña")
+let errorContrasena = document.getElementById("errorContrasena")
+
+let ojito = document.getElementById("ojito")
+
+ojito.addEventListener("click", function() {
+    if (contrasena.type === "password") {
+        contrasena.type = "text"
+        ojito.classList.remove("fa-eye")
+        ojito.classList.add("fa-eye-slash")
+    } else {
+        contrasena.type = "password"
+        ojito.classList.remove("fa-eye-slash")
+        ojito.classList.add("fa-eye")
+    }
+})
+
+
 
 // Esta función se ejecuta cuando el usuario hace clic en el botón
 boton.addEventListener("click", function(e) {
@@ -18,44 +35,80 @@ let valido = true
  if (nombre.value === "") {
         errorNombre.textContent = "⚠️ El nombre es obligatorio"
         errorNombre.style.color = "red"
+         nombre.style.borderBottom = "2px solid red"
+
+         nombre.classList.add("temblar")
+    setTimeout(function() { nombre.classList.remove("temblar") }, 300)
+
         valido = false
     } else {
         errorNombre.textContent = ""
+         nombre.style.borderBottom = ""
     }
 
     // --- VALIDACIÓN DEL APELLIDO ---
     if (apellido.value === "") {
         errorApellido.textContent = "⚠️ El apellido es obligatorio"
         errorApellido.style.color = "red"
+         apellido.style.borderBottom = "2px solid red"
+
+          apellido.classList.add("temblar")
+    setTimeout(function() { apellido.classList.remove("temblar") }, 300)
+
         valido = false
     } else {
         errorApellido.textContent = ""
+         apellido.style.borderBottom = ""
     }
 
     // --- VALIDACIÓN DEL CORREO ---
     if (correo.value === "") {
         errorCorreo.textContent = "⚠️ El correo es obligatorio"
         errorCorreo.style.color = "red"
+         correo.style.borderBottom = "2px solid red"
+
+          correo.classList.add("temblar")
+    setTimeout(function() { correo.classList.remove("temblar") }, 300)
+
         valido = false
     } else if (!correo.value.includes("@")) {
         errorCorreo.textContent = "⚠️ El correo debe tener @"
         errorCorreo.style.color = "red"
+        correo.style.borderBottom = "2px solid red"
+
+         correo.classList.add("temblar")
+    setTimeout(function() { correo.classList.remove("temblar") }, 300)
+
         valido = false
+
     } else {
         errorCorreo.textContent = ""
+         correo.style.borderBottom = ""
     }
 
     // --- VALIDACIÓN DE CONTRASEÑA ---
     if (contrasena.value === "") {
         errorContrasena.textContent = "⚠️ La contraseña es obligatoria"
         errorContrasena.style.color = "red"
+        contrasena.style.borderBottom = "2px solid red"
+
+         contrasena.classList.add("temblar")
+    setTimeout(function() { contrasena.classList.remove("temblar") }, 300)
+
         valido = false
     } else if (contrasena.value.length < 8) {
         errorContrasena.textContent = "⚠️ La contraseña debe tener mínimo 8 caracteres"
         errorContrasena.style.color = "red"
+        contrasena.style.borderBottom = "2px solid red"
+
+         contrasena.classList.add("temblar")
+    setTimeout(function() { contrasena.classList.remove("temblar") }, 300)
+
+
         valido = false
     } else {
         errorContrasena.textContent = ""
+         contrasena.style.borderBottom = ""
     }
 
     // ---  REDIRIGE AL LOGIN ---
